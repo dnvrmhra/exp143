4217e8e9d6ce8fc96d982f5e0e3dc21931f899fd
dnvrmhra <dnvrmhra@gmail.com>
2026-03-04 19:16:09 +0530

    Update booking-system.js

--- booking-system.js
+++ booking-system.js
@@ -44,6 +44,6 @@ app.post("/api/book", async (req, res) => {
 
 const PORT=process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log("Booking system running on port 3000");
+app.listen(PORT, () => {
+    console.log("Booking system running on port ${PORT}");
 });
