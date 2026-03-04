607bf763c3594a932353af062ba04fb1ccd933e0
dnvrmhra <dnvrmhra@gmail.com>
2026-03-04 15:15:14 +0530

    Update redisClient.js

--- redisClient.js
+++ redisClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "redis";
 const client = createClient({
-    url: "redis://127.0.0.1:6379"
+    url: process.env.REDIS_URL
 });
 client.on("error", (err) => console.log("Redis Error:", err));
 await client.connect();
