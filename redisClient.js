e46c9fe6e418f659cea45fe4e0d7b10cc4a7640c
dnvrmhra <dnvrmhra@gmail.com>
2026-03-04 19:14:56 +0530

    Update redisClient.js

--- redisClient.js
+++ redisClient.js
@@ -1,8 +1,22 @@
 import { createClient } from "redis";
+
+const redisUrl = process.env.REDIS_URL;
+
+if (!redisUrl) {
+  throw new Error("REDIS_URL environment variable not set");
+}
+
 const client = createClient({
-    url: process.env.REDIS_URL
+  url: redisUrl
 });
-client.on("error", (err) => console.log("Redis Error:", err));
-await client.connect();
-console.log("Redis connected");
+
+client.on("error", (err) => console.error("Redis Error:", err));
+
+async function connectRedis() {
+  await client.connect();
+  console.log("Redis connected");
+}
+
+await connectRedis();
+
 export default client;
